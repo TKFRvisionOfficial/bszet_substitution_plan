0ac6f4e13f4b6f501b0530a6e5a946b28b06623f
TKFRvisionOfficial <tkfrvision@gmail.com>
2021-09-12 19:43:31 +0200

    now using multiple pics and uuids

--- pdf_converter.py
+++ pdf_converter.py
@@ -1,16 +1,35 @@
 from typing import Optional
 from fastapi import FastAPI, UploadFile, File, Response, Request
-from util import convert_pdf_to_img
+from fastapi.responses import JSONResponse, FileResponse
+from starlette.background import BackgroundTasks
+from util import save_pdf_to_folder
 import os
 
 auth_key = f'Bearer {os.environ["AUTH_KEY"]}'
+image_path = "pictures"
 app = FastAPI()
 
 
 @app.post("/pdf2img")
 async def read_item(request: Request, file: UploadFile = File(...)):
+    # if True:
     if request.headers.get("Authorization") == auth_key:
-        image = convert_pdf_to_img(await file.read())
-        return Response(content=image, media_type="image/jpeg", status_code=200)
+        uuids = save_pdf_to_folder(await file.read(), image_path)
+        return JSONResponse(content=uuids,  status_code=200)
     else:
         return Response(content="Forbidden", status_code=403)
+
+
+@app.get("/img/{uuid}")
+async def get_file(uuid: str, request: Request, background_task: BackgroundTasks):
+    # if True:
+    if request.headers.get("Authorization") == auth_key:
+        file_path = os.path.join(image_path, uuid + ".jpg")
+        if os.path.exists(file_path):
+            background_task.add_task(os.remove, file_path)
+            return FileResponse(path=file_path, media_type="image/jpeg", status_code=200)
+        else:
+            return Response(content="Not Found", status_code=404)
+    else:
+        return Response(content="Forbidden", status_code=403)
+
--- util.py
+++ util.py
@@ -1,7 +1,9 @@
 import pdf2image
 from PIL import Image
 import io
-
+from uuid import uuid4
+from typing import List
+import os
 
 def convert_pdf_to_img(pdf: bytes) -> bytes:
     images = pdf2image.convert_from_bytes(pdf, 200)
@@ -20,3 +22,15 @@ def convert_pdf_to_img(pdf: bytes) -> bytes:
     byte_array = io.BytesIO()
     result_img.save(byte_array, format="JPEG", dpi=(200, 200), quality=90)
     return byte_array.getvalue()
+
+
+def save_pdf_to_folder(pdf: bytes, path: str) -> List[str]:
+    if not os.path.exists(path):
+        os.mkdir(path)
+    uuid_list = []
+    images = pdf2image.convert_from_bytes(pdf, 200)
+    for image in images:
+        cur_uuid = str(uuid4())
+        uuid_list.append(cur_uuid)
+        image.save(os.path.join(path, cur_uuid + ".jpg"), dpi=(200, 200), quality=90)
+    return uuid_list
