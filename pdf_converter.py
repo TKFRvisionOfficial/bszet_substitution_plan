2b956c4b79a2a64334bf77576bdc99c154f84a66
TKFRvisionOfficial <tkfrvision@gmail.com>
2021-09-12 02:20:37 +0200

    adding auth system

--- pdf_converter.py
+++ pdf_converter.py
@@ -1,11 +1,16 @@
 from typing import Optional
-from fastapi import FastAPI, UploadFile, File, Response
+from fastapi import FastAPI, UploadFile, File, Response, Request
 from util import convert_pdf_to_img
+import os
 
+auth_key = f'Bearer {os.environ["AUTH_KEY"]}'
 app = FastAPI()
 
 
 @app.post("/pdf2img")
-async def read_item(file: UploadFile = File(...)):
-    image = convert_pdf_to_img(await file.read())
-    return Response(content=image, media_type="image/jpeg")
+async def read_item(file: UploadFile = File(...), request: Request = Request(...)):
+    if request.headers.get("Authorization") == auth_key:
+        image = convert_pdf_to_img(await file.read())
+        return Response(content=image, media_type="image/jpeg", status_code=200)
+    else:
+        return Response(content="Forbidden", status_code=403)
