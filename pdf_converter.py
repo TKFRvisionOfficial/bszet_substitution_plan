72afeecb53b446e78934d5f8ceb3eb1aa2c1136a
TKFRvisionOfficial <tkfrvision@gmail.com>
2021-09-12 02:38:03 +0200

    fixing ellipsis error

--- pdf_converter.py
+++ pdf_converter.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 
 @app.post("/pdf2img")
-async def read_item(file: UploadFile = File(...), request: Request = Request(...)):
+async def read_item(request: Request, file: UploadFile = File(...)):
     if request.headers.get("Authorization") == auth_key:
         image = convert_pdf_to_img(await file.read())
         return Response(content=image, media_type="image/jpeg", status_code=200)
