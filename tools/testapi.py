0b90763776d3bf1b8ac75d1144cdd8945013f450
TKFRvisionOfficial <52245132+TKFRvisionOfficial@users.noreply.github.com>
2021-12-15 20:17:19 +0100

    fuck

--- tools/testapi.py
+++ tools/testapi.py
@@ -2,6 +2,6 @@ import requests
 import json
 
 with open(r"C:\Users\tkfrv\Downloads\nur_bilder.pdf", "rb") as file:
-	response_text = requests.post("***REMOVED***", files={'file': file}, headers={"Authorization": "Bearer ***REMOVED***"}).content.decode("utf-8")
+	response_text = requests.post("URL", files={'file': file}, headers={"Authorization": "Bearer API_KEY"}).content.decode("utf-8")
 
 print(response_text)
