b968149b0591a75b020921955837c6bd40fb3dd4
PBahner <kl.timo@outlook.de>
2021-12-19 14:38:29 +0100

    adding sort function which sorts recognized texts by their position

--- img_to_dataframe.py
+++ img_to_dataframe.py
@@ -3,6 +3,7 @@ import numpy as np
 from easyocr import Reader
 import pandas as pd
 import re
+from math import isclose
 
 # from: https://medium.com/analytics-vidhya/how-to-detect-tables-in-images-using-opencv-and-python-6a0f15e560c3
 langs = ["de", "en"]
@@ -22,18 +23,52 @@ def find_contours(img_gray):
     return contours
 
 
+def sort_and_join_texts(easyocr_result):
+    # sort recognized text by coordinates for right text order
+    sorted_by_xaxes = sorted(easyocr_result, key=lambda k: k[0][0][0])
+    # get all y-coordinates of texts
+    all_y_coordinates = [coordinate[0][0][1] for coordinate in sorted_by_xaxes]
+    # list for coordinates of lines, smallest coordinate is given
+    line_coordinates = [min(all_y_coordinates)]
+    # filter y-coordinates by close values (get all lines)
+    for y in all_y_coordinates:
+        close_to_existing_value = False
+        for line in line_coordinates:
+            if isclose(y, line, abs_tol=15):
+                close_to_existing_value = True
+                break
+        if not close_to_existing_value:
+            line_coordinates.append(y)
+    # sort lines
+    line_coordinates = sorted(line_coordinates)
+
+    # sort texts into 2D-list
+    sorted_text = [[] for _ in range(len(line_coordinates))]
+    for i, y in enumerate(all_y_coordinates):  # iterate all recognized texts
+        for line, line_coordinate in enumerate(line_coordinates):  # iterate all text-lines
+            if isclose(y, line_coordinate, abs_tol=15):  # match text to line
+                sorted_text[line].append(sorted_by_xaxes[i][1])  # insert text into list
+
+    recognized_text = " ".join(item for line in sorted_text for item in line)
+
+    return recognized_text
+
+
 def img_to_text(input_img):
     (part_height, part_width) = input_img.shape[:2]
     # preprocess image
     ret, img_bin = cv2.threshold(input_img, 150, 255, cv2.THRESH_BINARY)
-    img_bin = cv2.resize(img_bin, (part_width*10, part_height*10))
-    img_blurry_dark = cv2.resize(cv2.blur(255-img_bin, (5, 5)), (part_width*2, part_height*2))
+    img_bin = cv2.resize(img_bin, (part_width * 10, part_height * 10))
+    img_blurry_dark = cv2.resize(cv2.blur(255 - img_bin, (5, 5)), (part_width * 2, part_height * 2))
 
     # recognize inverted image
-    results_dark = reader.readtext(img_blurry_dark, detail=0)
-    recognized_text = "".join(results_dark)
+    results_dark = reader.readtext(img_blurry_dark)
 
-    # print(recognized_text)
+    if results_dark:
+        recognized_text = sort_and_join_texts(results_dark)  # sort texts
+    else:  # no text recognized
+        recognized_text = ""
+
     return recognized_text
 
 
@@ -119,13 +154,13 @@ def convert_table_img_to_list(img: np.ndarray):
     table.insert(0, table_row)  # insert last row
 
     # get img area where date can be
-    part_img = img_gray[date_upper_pos:table_upper_pos-60, table_left_pos:table_right_pos-300]
+    part_img = img_gray[date_upper_pos:table_upper_pos - 60, table_left_pos:table_right_pos - 300]
     date = img_to_text(part_img)  # get date from image
     # ToDo:
     # cv2 doesn't recognize Heading of Table because background is orange
-    # date+"\nKlasse" is intented because of compatibility to camelot
-    table.insert(0, [date+"\nKlasse", "Stunde", "Fach", "Raum", "Lehrkraft: +Vertretung / (fehlt)", "Mitteilung"])
-    
+    # date+"\nKlasse" is intended because of compatibility to camelot
+    table.insert(0, [date + "\nKlasse", "Stunde", "Fach", "Raum", "Lehrkraft: +Vertretung / (fehlt)", "Mitteilung"])
+
     data_frame = pd.DataFrame(table)
     data_frame = data_frame.fillna(value="")
     # print(data_frame)
